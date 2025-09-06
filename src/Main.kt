5448971e0c108c6b63532ef135076d6bdb1d0c55
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-09-05 23:26:08 -0300

    feat: data(Term, ResponseToken, TokenExpr, helpers(identify Operators) and logics functions(Operators, Tokenizer, Lexical and Syntaxe Analyser)

--- src/Main.kt
+++ src/Main.kt
@@ -1,117 +1,26 @@
+import logicFunctions.LexSynAnalyser
+import logicFunctions.Tokenizer
+import logicFunctions.TruthTableGenerator
+
 /**
  * You can edit, run, and share this code.
  * play.kotlinlang.org
  */
 
-enum class OperatorEnum(val order: Int){
-    OP_NOT(1),
-    OP_AND(2),
-    OP_OR(3),
-    OP_EXOR(3),
-    OP_THEN(4),
-    OP_EXTHEN(4)
-}
-
-enum class ExpOrder(val ordem: Int?) {
-    OPEN_PARENTHESE(0),
-    CLOSE_PARENTHESE(0),
-    OP_NOT(1),
-    OP_AND(2),
-    OP_OR(3),
-    OP_EXOR(3),
-    OP_THEN(4),
-    OP_EXTHEN(4),
-    TERM(5),
-    UNDERFINED(null)
-}
-
-data class TokenExp(val list: MutableList<ExpOrder>, val countOpenParenthese: Int, val countCloseParenthese: Int)
-
-fun tokenizerExp(exp: String): TokenExp {
-    // separa a expressao com uma lista de cada caractere
-    val expInArr = exp.replace(" ", "").trim().split("").drop(1).dropLast(1)
-    // lista tokenizada
-    val listOfExpressionOrder: MutableList<ExpOrder>  = mutableListOf()
-    var countOpen: Int = 0
-    var countClose: Int = 0
-    for(exp in expInArr){
-        when(exp){
-            "∧" -> {
-                listOfExpressionOrder.add(ExpOrder.OP_AND)
-            }
-            "→"  -> {
-                listOfExpressionOrder.add(ExpOrder.OP_THEN)
-            }
-            "+" -> listOfExpressionOrder.add(ExpOrder.OP_OR)
-            "(" -> {
-                countOpen += 1
-                listOfExpressionOrder.add(ExpOrder.OPEN_PARENTHESE)
-            }
-            ")" -> {
-                countClose += 1
-                listOfExpressionOrder.add(ExpOrder.CLOSE_PARENTHESE)
-            }
-            else -> {
-                if (exp.matches(Regex("^[a-zA-Z]$"))) {
-                    listOfExpressionOrder.add(ExpOrder.TERM)
-                } else {
-                    listOfExpressionOrder.add(ExpOrder.UNDERFINED)
-                }
-            }
-        }
-    }
-
-    return TokenExp(listOfExpressionOrder, countOpen, countClose)
-}
-
-fun isOperator(exp: ExpOrder?) = (exp == ExpOrder.OP_AND || exp == ExpOrder.OP_THEN || exp == ExpOrder.OP_OR)
-
-fun sintaxeAnalise(exp: String): Boolean {
-
-    val tokenizer: TokenExp = tokenizerExp(exp)
-    println(tokenizer)
-
-
-
-    var beforeExp: ExpOrder? = null
-
-    if(tokenizer.countOpenParenthese != tokenizer.countCloseParenthese){
-        println("Erro de sintaxe parenteses inconsistentes")
-        return false
-    }
-
-    for (token in tokenizer.list) {
-        val isValid = when {
-            token == ExpOrder.OPEN_PARENTHESE &&
-                    (beforeExp == null || isOperator(beforeExp) || beforeExp == ExpOrder.OPEN_PARENTHESE) -> true
-
-            token == ExpOrder.TERM &&
-                    (beforeExp == ExpOrder.OPEN_PARENTHESE || isOperator(beforeExp)) -> true
-
-            isOperator(token) &&
-                    (beforeExp == ExpOrder.TERM || beforeExp == ExpOrder.CLOSE_PARENTHESE) -> true
-
-            token == ExpOrder.CLOSE_PARENTHESE &&
-                    (beforeExp == ExpOrder.TERM || beforeExp == ExpOrder.CLOSE_PARENTHESE) -> true
-
-            else -> false
-        }
-
-        if (!isValid) {
-            println("Erro de sintaxe em: $token (anterior: $beforeExp)")
-            return false
-        }
-
-        println("OK: $token depois de $beforeExp")
-        beforeExp = token
-    }
-
-    return true
-}
 
 fun main() {
-    val exp = "(A ∧ B) → C + D"
+    val exp = "(A ∧ B) → C ∨ A"
 
-    print(sintaxeAnalise(exp))
+    val tokens = Tokenizer.generate(exp)
 
+//    println(tokens.listExpr)
+    val analyser = LexSynAnalyser.run(tokens)
+
+    val result = TruthTableGenerator.allCombinations(tokens.listTerm)
+
+//    print(result)
+//    for (r in result){
+//
+//        println("${r["A"]} and ${r["B"]} = ${logicFunctions.Operators.and(r["A"], r["B"])}");
+//    }
 }
---
+++ src/logicFunctions/LexSynAnalyser.kt
@@ -0,0 +1,45 @@
+package logicFunctions
+
+import data.ResponseToken
+import data.TokensExpr
+import helpers.isOperator
+
+object LexSynAnalyser {
+
+    fun run(tokenizer: ResponseToken): Boolean {
+        var beforeExp: TokensExpr? = null
+
+        if(tokenizer.countOpenParenthese != tokenizer.countCloseParenthese){
+            println("Erro de sintaxe parenteses inconsistentes")
+            return false
+        }
+
+        for (token in tokenizer.listExpr) {
+            val isValid = when {
+                token == TokensExpr.OPEN_PARENTHESE &&
+                        (beforeExp == null || isOperator(beforeExp) || beforeExp == TokensExpr.OPEN_PARENTHESE) -> true
+
+                token == TokensExpr.TERM &&
+                        (beforeExp == TokensExpr.OPEN_PARENTHESE || isOperator(beforeExp)) -> true
+
+                isOperator(token) &&
+                        (beforeExp == TokensExpr.TERM || beforeExp == TokensExpr.CLOSE_PARENTHESE) -> true
+
+                token == TokensExpr.CLOSE_PARENTHESE &&
+                        (beforeExp == TokensExpr.TERM || beforeExp == TokensExpr.CLOSE_PARENTHESE) -> true
+
+                else -> false
+            }
+
+            if (!isValid) {
+                println("Erro de sintaxe em: $token (anterior: $beforeExp)")
+                return false
+            }
+
+            println("OK: $token depois de $beforeExp")
+            beforeExp = token
+        }
+
+        return true
+    }
+}
