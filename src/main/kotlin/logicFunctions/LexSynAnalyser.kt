402ea7e4682bb55d192e5f1b60e620454cf56e0a
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-09-13 11:01:48 -0300

    fix: add validation next token

--- src/main/kotlin/logicFunctions/LexSynAnalyser.kt
+++ src/main/kotlin/logicFunctions/LexSynAnalyser.kt
@@ -2,36 +2,45 @@ package logicFunctions
 
 import data.ResponseToken
 import data.TokensExpr
-import helpers.isNotOperator
+import helpers.isEqualNotOperator
 import helpers.isOperator
 
 object LexSynAnalyser {
 
     fun run(tokenizer: ResponseToken): Boolean {
         var beforeExp: TokensExpr? = null
+        var afterExp: TokensExpr? = null
 
         if(tokenizer.countOpenParenthese != tokenizer.countCloseParenthese){
             println("Erro de sintaxe parenteses inconsistentes")
             return false
         }
 
-        for (token in tokenizer.listExpr) {
+        for (i in 0 until tokenizer.listExpr.size) {
+            var token = tokenizer.listExpr[i]
+            afterExp = if(tokenizer.listExpr.size != i + 1) tokenizer.listExpr[i + 1] else null
+
             val isValid = when {
-                token == TokensExpr.OPEN_PARENTHESE &&
-                        (beforeExp == null || isOperator(beforeExp) || beforeExp == TokensExpr.OPEN_PARENTHESE) -> true
+                token == TokensExpr.OPEN_PARENTHESE
+                        && (afterExp == TokensExpr.TERM || isEqualNotOperator(afterExp) || afterExp == TokensExpr.OPEN_PARENTHESE)
+                        && (beforeExp == null || isOperator(beforeExp) || beforeExp == TokensExpr.OPEN_PARENTHESE) -> true
 
-                token == TokensExpr.TERM &&
-                        (beforeExp == null || beforeExp == TokensExpr.OPEN_PARENTHESE || isOperator(beforeExp)) -> true
+                token == TokensExpr.TERM
+                        && (isOperator(afterExp) || afterExp == TokensExpr.CLOSE_PARENTHESE || afterExp == null)
+                        && (beforeExp == null || beforeExp == TokensExpr.OPEN_PARENTHESE || isOperator(beforeExp)) -> true
 
-                isOperator(token) &&
-                        (beforeExp == TokensExpr.TERM || beforeExp == TokensExpr.CLOSE_PARENTHESE) -> true
+                isOperator(token)
+                        && (afterExp == TokensExpr.TERM || afterExp == TokensExpr.OPEN_PARENTHESE || isEqualNotOperator(afterExp))
+                        && (beforeExp == TokensExpr.TERM || beforeExp == TokensExpr.CLOSE_PARENTHESE) -> true
 
-                isNotOperator(token) &&
-                        (beforeExp == TokensExpr.OPEN_PARENTHESE || isOperator(beforeExp) ||beforeExp == null)  -> true
+                isEqualNotOperator(token)
+                        && (afterExp == TokensExpr.TERM || afterExp == TokensExpr.OPEN_PARENTHESE)
+                        && (beforeExp == TokensExpr.OPEN_PARENTHESE || isOperator(beforeExp) ||beforeExp == null)  -> true
 
 
-                token == TokensExpr.CLOSE_PARENTHESE &&
-                        (beforeExp == TokensExpr.TERM || beforeExp == TokensExpr.CLOSE_PARENTHESE) -> true
+                token == TokensExpr.CLOSE_PARENTHESE
+                        && (afterExp == null || isOperator(afterExp) || afterExp == TokensExpr.CLOSE_PARENTHESE)
+                        && (beforeExp == TokensExpr.TERM || beforeExp == TokensExpr.CLOSE_PARENTHESE) -> true
 
                 else -> false
             }
@@ -41,7 +50,8 @@ object LexSynAnalyser {
                 return false
             }
 
-            println("OK: $token depois de $beforeExp")
+            println("OK: antes de $token vem $beforeExp")
+            println("OK: depois de $token vem $afterExp")
             beforeExp = token
         }
 
